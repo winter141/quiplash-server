f48cf2f4e26b308792f2c7ce7011d2d53075eb77
= <=>
2024-06-25 16:28:35 +1200

    Set up player questions for first round, not linked up yet

--- server/index.js
+++ server/index.js
@@ -14,9 +14,43 @@ const io = new Server(server, {
     }
 });
 
+const rooms= [{ roomCode: "LUCK", users: [] }]
+
+let isVIP = true;
+
 io.on("connection", (socket) => {
     console.log(`User Connected: ${socket.id}`);
 
+    // Game init for room
+    socket.on("init_game_room", (roomCode) => {
+        socket.join(roomCode);
+        isVIP = true;
+        console.log(roomCode);
+    })
+
+    socket.on("vip_start_game", (roomCode) => {
+        console.log(`Emitting to roomCode: ${roomCode}`);
+        socket.to(roomCode).emit("start_game");
+    })
+
+    // Users joining games
+    socket.on("join_room", (data) => {
+        const room = rooms.find(room => room.roomCode === data.room);
+        if (!room) {
+            // Error
+            console.log("Something went wrong.");
+            return;
+        }
+        room.users.push(data.username);
+        socket.join(data.room);
+        socket.to(data.room).emit("user_joined", data);
+        data.VIP = isVIP;
+        socket.emit("join_successful", data);
+        isVIP = false;
+        console.log(`${data.username} joined room ${data.room}`)
+    });
+
+    // Test
     socket.on("send_message", (data) => {
         socket.broadcast.emit("receive_message", data);
     })
